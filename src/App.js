33ee1afe21daf3c4279ecfe23a95471f714edaf3
TahsinulAmir <tahsinulamir1@gmail.com>
2022-07-22 22:56:45 +0700

    install route dom dan tambah hello

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import ManajemenBuku from './components/ManajemenBuku';
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Hello from './components/Hello';
+
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <ManajemenBuku />
+    <div>
+      <Router>
+        <Navbar />
+
+        <Switch>
+          <Route path='/hello'>
+            <Hello />
+          </Route>
+          <Route path='/manajemen-buku'>
+            <ManajemenBuku />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
-
 export default App;
